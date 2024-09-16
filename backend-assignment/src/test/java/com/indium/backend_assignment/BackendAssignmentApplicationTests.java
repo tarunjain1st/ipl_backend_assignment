4ac12a12a96e3f4c11289695236700cfad86dbe0
Tarun Kumar Jain <tarunjain1st@gmail.com>
2024-09-16 19:32:23 +0530

    spring boot application setup

---
+++ backend-assignment/src/test/java/com/indium/backend_assignment/BackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indium.backend_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
